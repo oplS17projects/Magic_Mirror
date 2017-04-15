fc186d1c8d2914c95938cc5c08c27638199336ef
kimste2 <steven_kim@student.uml.edu>
2017-04-15 14:45:18 -0400

    simple test
    tested step 1 with the weather in a new part of USA (SK)

--- OPL_FP.rkt
+++ OPL_FP.rkt
@@ -6,27 +6,28 @@
          ; web-server/templates
         ; 2htdp/image
          racket/date
+         (file "weather.rkt")
         ; racket/promise
          )
-(define list1 '(("city" . "Meredith")
-  ("current" . 50.58)
-  ("high" . 51.8)
-  ("low" . 50)
-  ("humidity" . 34)
-  ("wind" . 26.410761154855646)
-  ("description" . "Clouds")))
+;(define list1 '(("city" . "Meredith")
+;  ("current" . 50.58)
+;  ("high" . 51.8)
+;  ("low" . 50)
+;  ("humidity" . 34)
+;  ("wind" . 26.410761154855646)
+;  ("description" . "Clouds")))
 ; functions
 (define (finder word list)
  (cond ((null? list) "Nothing Found")
        ((and (pair? (car list))
             (string=? word (car(car list)))) (cdr(car list)))
        (else (finder word (cdr list)))))
-(define (adder a b) (+ a b))
+;(define (adder a b) (+ a b))
 ; test data 
-(define city (finder "city" list1))
-(define two 2)
-(define number_list (list 1 2 3 4 5))
-(define eight 8)
+;(define city (finder "city" list1))
+;(define two 2)
+;(define number_list (list 1 2 3 4 5))
+;(define eight 8)
 ; What are some funcitons that would make my life easier?
 ; finder funciton so i can ask for say "current" and get back the current temp
 
@@ -44,27 +45,39 @@
 (define (repl_newline)
   (display "\n"))
 (display "\n");
-(display (pic_weather (finder "city" list1)))
+;(display (pic_weather (finder "city" list1)))
 (display "\n")
 
 ;(number->string(finder "current" list1))
 (date-display-format 'american)
- (define title1 "Magic Mirror");
-(define (start req); start is a function that takes a request 
+ (define title1 "Magic Mirror Dynamic");
+; below is all hard coded 
+;(define (start req); start is a function that takes a request 
+;  (response/xexpr
+;   `(html (head (title "Magic Mirror")); this is the title of the webpage
+;          (body
+;           ;(left (h1 "I am above what you want to see."))(br)
+;           ;(center (h2 (string-append "test"  (number->string eight)))) ; should see an 8
+;           (left (h3 "City is: ",(finder "city" list1) (br)
+;                    "Humidity: ",(number->string(finder "humidity" list1)) (br)
+;                     "Current temp: " ,(number->string(finder "current" list1)) " Farenheit"(br) ; I want to see the number 50.58
+;                       ,(finder "description" list1))) (br)
+;            (left(img ([src "cloudy.png"]))); this outputs the picture
+;            
+;           ))))
+; THIS LIST IS HARD CODED FOR NOW 
+(define Dayton-OH( weather "current" "Dayton" "Ohio")) ; new data set 
+(define (start req)
   (response/xexpr
-   `(html (head (title "Magic Mirror")); this is the title of the webpage
-          (body
-           ;(left (h1 "I am above what you want to see."))(br)
-           ;(center (h2 (string-append "test"  (number->string eight)))) ; should see an 8
-           (left (h3 "City is: ",(finder "city" list1) (br)
-                    "Humidity: ",(number->string(finder "humidity" list1)) (br)
-                     "Current temp: " ,(number->string(finder "current" list1)) " Farenheit"(br) ; I want to see the number 50.58
-                       ,(finder "description" list1))) (br)
-            (left(img ([src "cloudy.png"]))); this outputs the picture
-            
-           ))))
-
-(display "Believe\n")
+   `(html(head (titile "Magic Mirror")); title of webpage; "`" is called a quasiqutoe 
+         (body
+          (left (h1 "This is above what you want to see.")) (br)
+          (left (h2 "City is: ", (finder "city" Dayton-OH))(br)
+                (h2 "Current temperature: ",(number->string (finder "current" Dayton-OH))))(br)
+                (left (img ([src ,(pic_weather (finder "description" Dayton-OH))])))                                                               
+          (h3 "The end")
+          ))))
+;(display "Believe\n")
          (serve/servlet start
 #:listen-ip #f ; accept connections from external machines 
 #:port 9000 ; change port number 
